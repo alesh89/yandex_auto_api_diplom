890515a2fa3bd88a5f4bdfc742b1212d5273fdab
Viktor Aleshinskiy <vintil89@gmail.com>
2024-08-15 17:34:07 +0300

    Удалены лишняя переменная и комментарий

--- configuration.py
+++ configuration.py
@@ -1,4 +1,3 @@
 URL_SERVICE = "https://8f399f2e-e76d-43df-b293-eeadad11fdc9.serverhub.praktikum-services.ru"
-DOC_PATH = "/docs/"
 CREATE_ORDERS="/api/v1/orders"
 TRACK_STATUS="/api/v1/orders/track"
--- create_order_test.py
+++ create_order_test.py
@@ -6,11 +6,10 @@ import data
 
 # Функция для проверки создания заказа
 def check_status(body):
-    # В переменной token_number сохранен 
     response_get_order = sender_stand_request.get_order()
     # Проверяется, что код ответа равен 200
     assert response_get_order.status_code == 200
 
-# Тест 1. Допустимое количество символов (1)
+# Проверям, что заказ был успешно создан
 def test_create_order_corect_data():
     check_status(data.order_body)
