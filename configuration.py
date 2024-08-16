7585e95bb456be514dd70b4cc84e26398742a98f
Viktor Aleshinskiy <vintil89@gmail.com>
2024-08-16 14:52:54 +0300

    Удалена лишняя функция из файла автотеста

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://30d12692-0de8-4276-a45a-dcebc5ebe402.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://7cd0b9fb-9528-494c-ade4-28afdb79fec3.serverhub.praktikum-services.ru"
 CREATE_ORDERS="/api/v1/orders"
 TRACK_STATUS="/api/v1/orders/track"
--- create_order_test.py
+++ create_order_test.py
@@ -4,11 +4,8 @@ import requests
 import sender_request
 import data
 
-def check_status(body):
+def test_create_order_corect_data():    
     track_number = sender_request.post_new_order(data.order_body)
     response_get_order = sender_request.get_order(track_number)
     # Проверяется, что код ответа равен 200
     assert response_get_order.status_code == 200
-
-def test_create_order_corect_data():    
-    check_status(data.order_body)
